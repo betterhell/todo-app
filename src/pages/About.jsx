2dd808ae842be07548e24f6c77b532a3319810c9
Dmitriy betterHell <skkogame@gmail.com>
2022-10-17 08:13:06 +0300

    About page text fixes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div className="flex flex-col items-center py-20">
+        <div className="flex flex-col justify-center items-center h-screen py-20">
             <h1 className="text-3xl font-bold mb-10">This is a simple ToDo React Application.</h1>
-            <p className="text-xl font-semibold">Creating: by <a className="underline hover:" href="https://github.com/betterhell">Me</a></p>
+            <p className="text-xl font-semibold">Creating: by
+                <a className="underline hover:text-red-500"
+                   href="https://github.com/betterhell">
+                    Me
+                </a>
+            </p>
         </div>
     );
 };
